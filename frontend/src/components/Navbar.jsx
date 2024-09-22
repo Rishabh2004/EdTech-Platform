e7335318840003a71f374e7f23f29daffc2f2529
Hem <hemnavadia@gmail.com>
2024-09-22 11:57:18 +0530

    Courses page add

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,11 +10,14 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="px-4 lg:px-6 bg-transparent backdrop-blur-sm shadow-md h-14 flex items-center justify-between">
+        <nav className="px-4 lg:px-6 bg-customBg text-customText backdrop-blur-sm shadow-md h-14 flex items-center justify-between">
             
             <div className="flex items-center">
                 <GraduationCap className="text-xl mr-3" />
-                <h1 className="text-lg font-semibold">EduPlatform</h1>
+                <Link to="/" className="text-lg font-medium hover:underline underline-offset-4">
+                    <h1 className="text-lg font-semibold">EduPlatform</h1>
+                </Link>
+                
             </div>
 
             <button
